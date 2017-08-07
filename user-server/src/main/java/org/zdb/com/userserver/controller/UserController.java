1dbef5b7cf0a9ac917b1da15dd92e743423132d0
dongbiao.zheng <dongbiao.zheng@renren-inc.com>
2017-08-07 19:29:26 +0800

    提交bug

--- user-server/src/main/java/org/zdb/com/userserver/controller/UserController.java
+++ user-server/src/main/java/org/zdb/com/userserver/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/user")
-@RefreshScope
 public class UserController {
     @Value("${profile}")
     private String profile;
