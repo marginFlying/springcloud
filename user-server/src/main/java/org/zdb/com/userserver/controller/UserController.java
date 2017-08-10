b5f13ba8c089757b8ef07a6007f0c406892c8453
dongbiao.zheng <dongbiao.zheng@renren-inc.com>
2017-08-10 10:56:26 +0800

    提交消息总线更新配置信息

--- user-server/src/main/java/org/zdb/com/userserver/controller/UserController.java
+++ user-server/src/main/java/org/zdb/com/userserver/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
     @Value("${serverUrl}")
     private String serverUrl;
