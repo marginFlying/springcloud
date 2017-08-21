f855b25e185e4cdbad41086a8a3fb758077891b4
dongbiao.zheng <dongbiao.zheng@renren-inc.com>
2017-08-21 14:24:02 +0800

    给rabbit添加虚拟主机

---
+++ stream-transform-server/src/main/java/org/zdb/com/streamtransformserver/StreamTransformServerApplication.java
@@ -0,0 +1,12 @@
+package org.zdb.com.streamtransformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamTransformServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamTransformServerApplication.class, args);
+	}
+}
---
+++ stream-transform-server/src/test/java/org/zdb/com/streamtransformserver/StreamTransformServerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zdb.com.streamtransformserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreamTransformServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
